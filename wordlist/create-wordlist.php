4c747490b4d3678b262251e6a833351896bacd99
PMK555 <pasanlokuge@gmail.com>
2020-04-26 22:57:53 +0530

    fix 4

--- wordlist/create-wordlist.php
+++ wordlist/create-wordlist.php
@@ -95,8 +95,8 @@ function readWordListPeterNorvig($filename, $dice) {
 
 	$retval = array();
 
-	$fp = @fopen_s($filename, "r");
-	if (!$fp) {
+	$fpfile = @fopen_s($filename, "r");
+	if (!$fpfile) {
 		throw new Exception("Could not open '$filename' for reading");
 	}
 
@@ -118,7 +118,7 @@ function readWordListPeterNorvig($filename, $dice) {
 		7 => array("min" => 4, "max" => 11),
 		);
 
-	while ($line = fgets($fp)) {
+	while ($line = fgets($fpfile)) {
 
 		$line = rtrim($line);
 		list($word, $freq) = explode("\t", $line);
@@ -143,7 +143,7 @@ function readWordListPeterNorvig($filename, $dice) {
 	//
 	sort($retval);
 
-	fclose($fp);
+	fclose($fpfile);
 
 	return($retval);
 
